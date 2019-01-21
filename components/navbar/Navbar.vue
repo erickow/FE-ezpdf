aa73684e384fd27c8006540ae0b0667ee2045715
Ericko Wicaksono <ericko.wicaksono@gmail.com>
2019-01-21 18:25:08 +0700

    styling home page

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -1,32 +1,20 @@
 <template>
-  <div class="container fullhd">
-    <section class="hero is-danger">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">
-            PDF CONVERT
-          </h1>
-          <h2 class="subtitle">
-            PDF Split, Join easily
-          </h2>
-        </div>
-      </div>
-    </section>
-    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="container">
+    <nav class="navbar is-danger" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <a class="navbar-item" href="">
           <!-- <img src="" width="112" height="28"> -->
-          <h1>PdfConvert</h1>
+          <h1>&nbsp;&nbsp;&nbsp;&nbsp;PdfConvert</h1>
         </a>
 
-        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <a role="button" class="navbar-burger " aria-label="menu" aria-expanded=" " data-target="navbarHeader">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
 
-      <div id="navbarBasicExample" class="navbar-menu">
+      <div id="navbarHeader" class="navbar-menu">
         <div class="navbar-start">
           <a class="navbar-item" href="/">
             Home
@@ -79,6 +67,33 @@
 
 <script>
   export default {
+    mounted() {
+      document.addEventListener('DOMContentLoaded', () => {
+
+        // Get all "navbar-burger" elements
+        const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+        // Check if there are any navbar burgers
+        if ($navbarBurgers.length > 0) {
+
+          // Add a click event on each of them
+          $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+              // Get the target from the "data-target" attribute
+              const target = el.dataset.target;
+              const $target = document.getElementById(target);
+
+              // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+              el.classList.toggle('is-active');
+              $target.classList.toggle('is-active');
+
+            });
+          });
+        }
+
+      });
+    },
     data() {
       return {
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,73 @@
 <template>
-  <section class="container is-fluid">
-    <div>
-      test
-    </div>
+  <section class="container homePage">
+    <section class="hero is-danger">
+      <div class="hero-body">
+        <div class="container">
+          <div class="columns">
+            <div class="column">
+              <img src="@/assets/image/pdf-icon.png" alt="pdf image">
+            </div>
+            <div class="column rightside-home has-text-justify">
+              <h1 class="title column is-10">
+                PDF CONVERT
+              </h1>
+              <h2 class="subtitle column is-10">
+                Easily Split and join your pdf files
+              </h2>
+              <div class="columns button-custom">
+                <div class="column is-5">
+                  <a href="/split" class="button is-danger is-inverted is-outlined is-large is-fullwidth">
+                    Split PDF</a>
+                </div>
+                <div class="column is-5">
+                  <a href="/join" class="button is-danger is-inverted is-outlined is-large is-fullwidth">Join PDF</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+  import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
+  export default {
+    components: {
+      Logo
+    }
   }
-}
+
 </script>
 
-<style>
+<style scoped>
+  .homePage {
+    align-content: center;
+    text-align: center;
+  }
+
+  .button-custom {
+    margin-top: 10vh;
+  }
+
+  @media screen and (min-width: 550px) {
+    .title {
+      font-size: 2.32rem;
+    }
+  }
+
+  @media screen and (min-width: 769px) {
+    .rightside-home {
+      margin-top: 10vh;
+    }
+  }
+
+  @media screen and (min-width: 1088px) {
+    .title {
+      font-size: 3.5rem;
+    }
+  }
 
 </style>
